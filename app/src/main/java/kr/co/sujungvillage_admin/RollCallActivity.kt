3825e0ce8b9b7dc425822c933f95a0242422a784
flora7883 <flora7883@gmail.com>
2022-07-29 22:45:42 +0900

    [MOD/#20]xml 수정. 버튼 연결

--- app/src/main/java/kr/co/sujungvillage_admin/RollCallActivity.kt
+++ app/src/main/java/kr/co/sujungvillage_admin/RollCallActivity.kt
@@ -2,10 +2,17 @@ package kr.co.sujungvillage_admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.sujungvillage_admin.databinding.ActivityCommWriteBinding
+import kr.co.sujungvillage_admin.databinding.ActivityRollCallBinding
 
 class RollCallActivity : AppCompatActivity() {
+    val binding by lazy { ActivityRollCallBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_roll_call)
+        setContentView(binding.root)
+
+        // 뒤로가기 버튼 연결
+        binding.btnBack.setOnClickListener{ finish() }
     }
 }
