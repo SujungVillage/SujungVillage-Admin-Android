179773b669cd71574ec0383b46756eab0e87cef9
flora7883 <flora7883@gmail.com>
2022-07-29 23:20:20 +0900

    [MOD]xml 수정, back 버튼 연결

--- app/src/main/java/kr/co/sujungvillage_admin/CommDetailActivity.kt
+++ app/src/main/java/kr/co/sujungvillage_admin/CommDetailActivity.kt
@@ -2,10 +2,17 @@ package kr.co.sujungvillage_admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.sujungvillage_admin.databinding.ActivityCommDetailBinding
+import kr.co.sujungvillage_admin.databinding.ActivityCommWriteBinding
 
 class CommDetailActivity : AppCompatActivity() {
+    val binding by lazy { ActivityCommDetailBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_comm_detail)
+        setContentView(binding.root)
+
+        // 뒤로가기 버튼 연결
+        binding.btnBack.setOnClickListener{ finish() }
     }
 }
