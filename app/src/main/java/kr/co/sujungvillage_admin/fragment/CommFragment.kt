2861ba5b7b1f768e892e49d10ef2a9d112394312
b1urrrr <b1urrr@naver.com>
2022-08-07 15:57:59 +0900

    [MOD/#17] 알림 레이아웃 개선

--- app/src/main/java/kr/co/sujungvillage_admin/fragment/CommFragment.kt
+++ app/src/main/java/kr/co/sujungvillage_admin/fragment/CommFragment.kt
@@ -8,24 +8,29 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import kr.co.sujungvillage_admin.CommWriteActivity
-import kr.co.sujungvillage_admin.MainActivity
-import kr.co.sujungvillage_admin.NoticeActivity
-import kr.co.sujungvillage_admin.R
-import kr.co.sujungvillage_admin.databinding.FragmentCommunityBinding
+import kr.co.sujungvillage_admin.*
+import kr.co.sujungvillage_admin.databinding.FragmentCommBinding
 
 class CommFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = FragmentCommunityBinding.inflate(inflater, container, false)
+        val binding = FragmentCommBinding.inflate(inflater, container, false)
+
+        // 기숙사 스피너 연결 및 커스텀
+        binding.spinnerDormitory.adapter = ArrayAdapter.createFromResource(requireContext(), R.array.dormitory, R.layout.spinner_comm_dormitory)
+
+        // 알림 버튼 연결
+        binding.btnAlarm.setOnClickListener {
+            var intent = Intent(this.activity, AlarmActivity::class.java)
+            startActivity(intent)
+        }
 
         // 글 작성 버튼 연결
         binding.btnWrite.setOnClickListener {
             var intent = Intent(this.activity, CommWriteActivity::class.java)
             startActivity(intent)
         }
-        // 기숙사 스피너 연결 및 커스텀
-        binding.spinnerDormitory.adapter = ArrayAdapter.createFromResource(requireContext(), R.array.dormitory, R.layout.spinner_comm_dormitory)
+
         return binding.root
     }
 }
